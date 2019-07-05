880345d65b6133e19c614101377ef9f50366c463
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-07-05 17:55:52 +0100

    make AoC012017 more kotlinic

--- src/main/kotlin/com/adventofcode/aoc2017/AoC012017.kt
+++ src/main/kotlin/com/adventofcode/aoc2017/AoC012017.kt
@@ -6,27 +6,23 @@ import java.util.stream.Stream
 
 class AoC012017 : Solution {
 
-    private fun solve(s: String, computeNext: (Int) -> Int): String {
+    private fun String.sum(computeNext: (Int) -> Int): String {
         var res = 0
-        for (i in 0 until s.length) {
+        for (i in 0 until length) {
             //compute where is the value to check
-            val next = computeNext.invoke(i)
-            val toCheck = s[next]
-            val curr = s[i]
+            val next = computeNext(i)
+            val toCheck = this[next]
+            val curr = this[i]
             if (curr == toCheck) {
                 res += charToInt(curr)
             }
         }
-        return itoa(res.toLong())
+        return itoa(res)
     }
 
-    override fun solveFirstPart(input: Stream<String>): String {
-        val s = getFirstString(input)
-        return solve(s) { i -> (i + 1) % s.length }
-    }
+    override fun solveFirstPart(input: Stream<String>): String =
+            with(getFirstString(input)) { sum { (it + 1) % length } }
 
-    override fun solveSecondPart(input: Stream<String>): String {
-        val s = getFirstString(input)
-        return solve(s) { i -> (i + s.length / 2) % s.length }
-    }
+    override fun solveSecondPart(input: Stream<String>): String =
+            with(getFirstString(input)) { sum { (it + length / 2) % length } }
 }
