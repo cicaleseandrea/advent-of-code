7abe0e814e3c8dffe6a0c943ac964b5403f0bfc7
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-11-01 12:19:45 +0000

    here is some garbage

--- src/main/java/com/adventofcode/aoc2017/AoC092017.java
+++ src/main/java/com/adventofcode/aoc2017/AoC092017.java
@@ -5,19 +5,61 @@ import com.adventofcode.Solution;
 import java.util.stream.Stream;
 
 import static com.adventofcode.utils.Utils.getFirstString;
+import static com.adventofcode.utils.Utils.itoa;
 
 class AoC092017 implements Solution {
 
-    //TODO
-    private static String solve(final Stream<String> input) {
-        return getFirstString(input);
+    private static String solve(final String input, boolean first) {
+        boolean deleteThisChar = false;
+        boolean isGarbage = false;
+        long garbageCount = 0L;
+        long currentGroupScore = 0L;
+        long totalGroupScore = 0L;
+        for (final char c : input.toCharArray()) {
+            if (deleteThisChar || deleteNextChar(c)) {
+                //if delete this char, flip back to false
+                //if delete next char, flip to true
+                deleteThisChar = !deleteThisChar;
+            } else if (isGarbage && (isGarbage = !endGarbage(c))) {
+                //inside garbage
+                garbageCount++;
+            } else if (startGarbage(c)) {
+                isGarbage = true;
+            } else if (startGroup(c)) {
+                currentGroupScore++;
+            } else if (endGroup(c)) {
+                totalGroupScore += currentGroupScore--;
+            }
+        }
+        return first ? itoa(totalGroupScore) : itoa(garbageCount);
+    }
+
+    private static boolean startGroup(char c) {
+        return c == '{';
+    }
+
+    private static boolean endGroup(char c) {
+        return c == '}';
+    }
+
+    private static boolean startGarbage(char c) {
+        return c == '<';
+    }
+
+    private static boolean endGarbage(char c) {
+        return c == '>';
+    }
+
+    private static boolean deleteNextChar(char c) {
+        return c == '!';
     }
 
     public String solveFirstPart(final Stream<String> input) {
-        return solve(input);
+        return solve(getFirstString(input), true);
     }
 
     public String solveSecondPart(final Stream<String> input) {
-        return solve(input);
+        return solve(getFirstString(input), false);
     }
+
 }
