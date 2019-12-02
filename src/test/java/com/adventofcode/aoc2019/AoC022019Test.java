0eaf2d6fdc74f174c1b25c45059face222b54924
Andrea Cicalese <and.cicalese@gmail.com>
2019-12-02 23:54:15 +0000

    🔨🛠️🔧

--- src/test/java/com/adventofcode/aoc2019/AoC022019Test.java
+++ src/test/java/com/adventofcode/aoc2019/AoC022019Test.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import static com.adventofcode.Generic.Type.FIRST;
 import static com.adventofcode.Generic.Type.SECOND;
-import static com.adventofcode.Solution.getInput;
 
 @RunWith(Parameterized.class)
 public class AoC022019Test extends Generic {
