bdcb072cc27dc0fc65870f78583946e1265cc4b9
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-07 01:06:13 +0000

    🔌🧵⚡

--- src/main/java/com/adventofcode/aoc2019/AoC032019.java
+++ src/main/java/com/adventofcode/aoc2019/AoC032019.java
@@ -8,9 +8,7 @@ import static com.adventofcode.utils.Utils.manhattanDistance;
 import static com.adventofcode.utils.Utils.splitOnRegex;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -26,31 +24,29 @@ class AoC032019 implements Solution {
 	public String solveFirstPart( final Stream<String> input ) {
 		return solve( input,
 				//distance from center
-				( firstWire, secondWire ) -> ( intersection ) -> manhattanDistance( Pair.ZERO,
+				( a, b ) -> ( intersection ) -> manhattanDistance( Pair.ZERO,
 						intersection ) );
 	}
 
 	public String solveSecondPart( final Stream<String> input ) {
 		return solve( input,
 				//sum of steps
-				( firstWire, secondWire ) -> ( intersection ) -> firstWire.get(
-						intersection ) + secondWire.get( intersection ) );
+				( firstWireSteps, secondWireSteps ) -> ( intersection ) -> firstWireSteps.get(
+						intersection ) + secondWireSteps.get( intersection ) );
 	}
 
 	private String solve( final Stream<String> input,
 			final BiFunction<Map<Pair<Long, Long>, Long>, Map<Pair<Long, Long>, Long>, ToLongFunction<Pair<Long, Long>>> function ) {
 		final var directions = input.collect( toList() );
 
-		final Set<Pair<Long, Long>> firstWire = new HashSet<>();
 		final Map<Pair<Long, Long>, Long> firstWireSteps = new HashMap<>();
-		initializeWire( directions.get( 0 ), firstWire, firstWireSteps );
+		initializeWire( directions.get( 0 ), firstWireSteps );
 
-		final Set<Pair<Long, Long>> secondWire = new HashSet<>();
 		final Map<Pair<Long, Long>, Long> secondWireSteps = new HashMap<>();
-		initializeWire( directions.get( 1 ), secondWire, secondWireSteps );
+		initializeWire( directions.get( 1 ), secondWireSteps );
 
 		final var curriedFunction = function.apply( firstWireSteps, secondWireSteps );
-		final Long min = Sets.intersection( firstWire, secondWire )
+		final Long min = Sets.intersection( firstWireSteps.keySet(), secondWireSteps.keySet() )
 				.stream()
 				.mapToLong( curriedFunction )
 				.min()
@@ -58,7 +54,7 @@ class AoC032019 implements Solution {
 		return itoa( min );
 	}
 
-	private void initializeWire( final String directions, final Set<Pair<Long, Long>> wire,
+	private void initializeWire( final String directions,
 			final Map<Pair<Long, Long>, Long> wireSteps ) {
 		final Pair<Long, Long> start = new Pair<>( 0L, 0L );
 		final AtomicLong steps = new AtomicLong();
@@ -71,18 +67,16 @@ class AoC032019 implements Solution {
 				case 'R' -> it -> it.setFirst( it.getFirst() + 1 );
 				default -> throw new IllegalStateException();
 			};
-			followDirection( direction, wire, wireSteps, start, steps, move );
+			followDirection( direction, wireSteps, start, steps, move );
 		} );
 	}
 
-	private void followDirection( final String direction, final Set<Pair<Long, Long>> wire,
-			final Map<Pair<Long, Long>, Long> wireSteps,
-			final Pair<Long, Long> current, final AtomicLong steps,
-			final Consumer<Pair<Long, Long>> move ) {
+	private void followDirection( final String direction,
+			final Map<Pair<Long, Long>, Long> wireSteps, final Pair<Long, Long> current,
+			final AtomicLong steps, final Consumer<Pair<Long, Long>> move ) {
 		for ( int i = 0; i < extractIntegerFromString( direction ); i++ ) {
 			move.accept( current );
 			final Pair<Long, Long> point = new Pair<>( current.getFirst(), current.getSecond() );
-			wire.add( point );
 			wireSteps.put( point, steps.incrementAndGet() );
 		}
 	}
