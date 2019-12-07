085150f886edfbdcc6cba95a32e1afe7bbb38497
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-07 22:41:49 +0000

    🔌🧵⚡

--- src/test/java/com/adventofcode/aoc2019/AoC032019Test.java
+++ src/test/java/com/adventofcode/aoc2019/AoC032019Test.java
@@ -23,20 +23,23 @@ public class AoC032019Test extends Generic {
     @Parameterized.Parameters(name = PARAMETERS_MESSAGE)
     public static Iterable<Object[]> data() {
         return List.of(new Object[][]{
-                {FIRST, "R8,U5,L5,D3\n" +
-                        "U7,R6,D4,L4", "6"},
-                {FIRST, "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" +
-                        "U62,R66,U55,R34,D71,R55,D58,R83", "159"},
-                {FIRST, "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" +
-                        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", "135"},
-                {FIRST, getInput(INSTANCE), "1225"},
-                {SECOND, "R8,U5,L5,D3\n" +
-                        "U7,R6,D4,L4", "30"},
-                { SECOND, "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" +
-                        "U62,R66,U55,R34,D71,R55,D58,R83", "610"},
-                { SECOND, "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" +
-                        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", "410"},
-                {SECOND, getInput(INSTANCE), "107036"}
+                { FIRST, "R8,U5,L5,D3\n" +
+                        "U7,R6,D4,L4", "6" },
+                { FIRST,
+                  "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" + "U62,R66,U55,R34,D71,R55,D58,R83",
+                  "159" },
+                { FIRST,
+                  "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" + "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+                  "135" },
+                { FIRST, getInput( INSTANCE ), "1225" },
+                { SECOND, "R8,U5,L5,D3\n" + "U7,R6,D4,L4", "30" },
+                { SECOND,
+                  "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" + "U62,R66,U55,R34,D71,R55,D58,R83",
+                  "610" },
+                { SECOND,
+                  "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" + "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+                  "410" },
+                { SECOND, getInput( INSTANCE ), "107036" }
         });
     }
 }
