60293cd4abef18a9da6c3c596638952fcad8275a
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-08 00:54:54 +0000

    📟🔑✅

--- src/main/java/com/adventofcode/aoc2019/AoC042019.java
+++ src/main/java/com/adventofcode/aoc2019/AoC042019.java
@@ -2,14 +2,16 @@ package com.adventofcode.aoc2019;
 
 import static com.adventofcode.utils.Utils.getFirstString;
 import static com.adventofcode.utils.Utils.itoa;
-import static com.adventofcode.utils.Utils.toDigitsArray;
 import static com.adventofcode.utils.Utils.toPositiveLongList;
 
 import java.util.List;
 import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
+import com.adventofcode.utils.Utils;
 
 class AoC042019 implements Solution {
 	public String solveFirstPart( final Stream<String> input ) {
@@ -21,35 +23,36 @@ class AoC042019 implements Solution {
 	}
 
 	public String solve( final Stream<String> input, final IntPredicate checkSize ) {
-		long res = 0;
 		final List<Long> range = toPositiveLongList( getFirstString( input ) );
-		for ( long i = range.get( 0 ); i < range.get( 1 ); i++ ) {
-			if ( acceptable( toDigitsArray( i ), checkSize ) ) {
-				res++;
-			}
-		}
+		long res = LongStream.range( range.get( 0 ), range.get( 1 ) )
+				.parallel()
+				.mapToObj( Utils::toDigitsArray )
+				.filter( acceptable( checkSize ) )
+				.count();
 		return itoa( res );
 	}
 
-	private boolean acceptable( final int[] arr, final IntPredicate checkSize ) {
-		boolean acceptable = false;
-		int groupSize = 1;
-		for ( int j = 1; j <= arr.length; j++ ) {
-			if ( j == arr.length || arr[j] > arr[j - 1] ) {
-				//no more digits or digit increasing: check latest group size
-				if ( checkSize.test( groupSize ) ) {
-					acceptable = true;
+	private Predicate<int[]> acceptable( final IntPredicate checkSize ) {
+		return ( int[] arr ) -> {
+			boolean acceptable = false;
+			int groupSize = 1;
+			for ( int j = 1; j <= arr.length; j++ ) {
+				if ( j == arr.length || arr[j] > arr[j - 1] ) {
+					//no more digits or digit increasing: check latest group size
+					if ( checkSize.test( groupSize ) ) {
+						acceptable = true;
+					}
+					groupSize = 1;
+				} else if ( arr[j] == arr[j - 1] ) {
+					//same digit, increase current group size
+					groupSize++;
+				} else {
+					//digit decreasing: unacceptable
+					acceptable = false;
+					break;
 				}
-				groupSize = 1;
-			} else if ( arr[j] == arr[j - 1] ) {
-				//same digit, increase current group size
-				groupSize++;
-			} else {
-				//digit decreasing: unacceptable
-				acceptable = false;
-				break;
 			}
-		}
-		return acceptable;
+			return acceptable;
+		};
 	}
 }
