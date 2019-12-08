aba676963875e3e3c3737f1852346dda738c3cbe
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-08 15:01:03 +0000

    1️2️0️2️🖥️🧙🔥

--- src/main/java/com/adventofcode/aoc2019/AoC022019.java
+++ src/main/java/com/adventofcode/aoc2019/AoC022019.java
@@ -13,23 +13,15 @@ import com.adventofcode.utils.Computer2019;
 class AoC022019 implements Solution {
 
 	public String solveFirstPart( final Stream<String> input ) {
-		final Computer2019 computer = new Computer2019();
-		final List<Long> program = setProgramInputs( toLongList( getFirstString( input ) ), 12L,
-				2L );
-		computer.loadProgram( program );
-		computer.run();
-		return itoa( computer.memory.get( 0 ) );
+		final List<Long> program = toLongList( getFirstString( input ) );
+		return itoa( computeResult( program, 12, 2 ) );
 	}
 
 	public String solveSecondPart( final Stream<String> input ) {
-		final Computer2019 computer = new Computer2019();
 		final List<Long> program = toLongList( getFirstString( input ) );
 		for ( int noun = 0; noun <= 99; noun++ ) {
 			for ( int verb = 0; verb <= 99; verb++ ) {
-				computer.loadProgram( setProgramInputs( program, noun, verb ) );
-				computer.setPointer( 0 );
-				computer.run();
-				if ( computer.memory.get( 0 ) == 19690720 ) {
+				if ( computeResult( program, noun, verb ) == 19690720 ) {
 					return itoa( 100 * noun + verb );
 				}
 			}
@@ -37,13 +29,15 @@ class AoC022019 implements Solution {
 		throw new IllegalStateException();
 	}
 
-	private List<Long> setProgramInputs( final List<Long> memory, final long noun,
-			final long verb ) {
-		if ( memory.size() > 10 ) {
-			memory.set( 1, noun );
-			memory.set( 2, verb );
+	private Long computeResult( final List<Long> program, final long noun, final long verb ) {
+		if ( program.size() > 10 ) {
+			program.set( 1, noun );
+			program.set( 2, verb );
 		}
-		return memory;
+		final Computer2019 computer = new Computer2019();
+		computer.loadProgram( program );
+		computer.run();
+		return computer.memory.get( 0 );
 	}
 
 }
