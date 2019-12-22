df7277c89305f5073ed250bb4dfb53a20cba85d5
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-22 15:57:47 +0000

    🦘🐇🦗

---
+++ src/test/java/com/adventofcode/aoc2019/AoC212019Test.java
@@ -0,0 +1,30 @@
+package com.adventofcode.aoc2019;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC212019Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC212019();
+
+    public AoC212019Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameterized.Parameters(name = PARAMETERS_MESSAGE)
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                { FIRST, getInput( INSTANCE ), "19354392" },
+                { SECOND, getInput( INSTANCE ), "1139528802" }
+        });
+    }
+}
