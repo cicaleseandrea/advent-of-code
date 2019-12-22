cf01702dfd79ffc3f4da8dcb27b0a9c050d34fab
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-22 23:42:08 +0000

    🍩♇↕️

--- src/main/java/com/adventofcode/aoc2019/AoC202019.java
+++ src/main/java/com/adventofcode/aoc2019/AoC202019.java
@@ -1,27 +1,23 @@
 package com.adventofcode.aoc2019;
 
+import static java.lang.Character.isLetter;
+
 import static com.adventofcode.utils.Pair.ZERO;
 import static com.adventofcode.utils.Utils.DOT;
-import static com.adventofcode.utils.Utils.EMPTY;
 import static com.adventofcode.utils.Utils.HASH;
 import static com.adventofcode.utils.Utils.itoa;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
 import com.adventofcode.utils.Pair;
-import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multimap;
 
 class AoC202019 implements Solution {
@@ -35,9 +31,9 @@ class AoC202019 implements Solution {
 	}
 
 	private String solve( final Stream<String> input, final boolean first ) {
-		final Pair<Pair<Long, Long>, Long> src = new Pair<>( ZERO, 0L );
-		final Pair<Pair<Long, Long>, Long> dst = new Pair<>( ZERO, 0L );
-		final var edges = initializeGrid( input, first, src, dst );
+		final var src = new Pair<>( ZERO, 0L );
+		final var dst = new Pair<>( ZERO, 0L );
+		final var edges = initialize( input, first, src, dst );
 		final long res = computeDistance( edges, src, dst );
 
 		return itoa( res );
@@ -49,10 +45,8 @@ class AoC202019 implements Solution {
 		//BFS to find shortest path (unweighted graphs, no need for Dijkstra)
 		final Queue<Pair<Pair<Long, Long>, Long>> queue = new LinkedList<>();
 		final Map<Pair<Pair<Long, Long>, Long>, Long> distances = new HashMap<>();
-		final Set<Pair<Pair<Long, Long>, Long>> seen = new HashSet<>();
 		//start from source
 		queue.add( src );
-		seen.add( src );
 		distances.put( src, 0L );
 		while ( !queue.isEmpty() ) {
 			//remove current node
@@ -62,18 +56,16 @@ class AoC202019 implements Solution {
 			}
 			for ( final var n : edges.get( curr.getFirst() ) ) {
 				//compute floor of this cell
-				final Long floor = curr.getSecond() + n.getSecond();
+				final long floor = curr.getSecond() + n.getSecond();
 				if ( floor == -1 ) {
 					//this door is blocked
 					continue;
 				}
 				//first time you see this cell
-				final var cell = new Pair<>( n.getFirst(), floor );
-				if ( !seen.contains( cell ) ) {
+				final var cell = ( curr.getSecond() == 0 ) ? n : new Pair<>( n.getFirst(), floor );
+				if ( !distances.containsKey( cell ) ) {
 					//add cell to the queue
 					queue.add( cell );
-					//cell was discovered
-					seen.add( cell );
 					//add distance from source
 					distances.put( cell, distances.get( curr ) + 1 );
 				}
@@ -82,23 +74,17 @@ class AoC202019 implements Solution {
 		throw new IllegalStateException();
 	}
 
-	private Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> initializeGrid(
+	private Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> initialize(
 			final Stream<String> input, final boolean first,
 			final Pair<Pair<Long, Long>, Long> src, final Pair<Pair<Long, Long>, Long> dst ) {
+
+		final Map<Pair<Long, Long>, Character> map = initializeMap( input );
+
+		final long maxX = map.keySet().stream().mapToLong( Pair::getFirst ).max().orElseThrow();
+		final long maxY = map.keySet().stream().mapToLong( Pair::getSecond ).max().orElseThrow();
+
 		final Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> edges = HashMultimap.create();
-		final ListMultimap<String, Pair<Long, Long>> portals = ArrayListMultimap.create();
-		final Map<Pair<Long, Long>, Character> map = new HashMap<>();
-
-		long y = 0;
-		for ( final String line : input.collect( Collectors.toList() ) ) {
-			long x = 0;
-			for ( final Character c : line.toCharArray() ) {
-				map.put( new Pair<>( x, y ), c );
-				x++;
-			}
-			y++;
-		}
-
+		final Map<String, Pair<Long, Long>> portals = new HashMap<>();
 		for ( final var p : map.entrySet() ) {
 			//for normal cells
 			if ( p.getValue() == DOT ) {
@@ -107,73 +93,102 @@ class AoC202019 implements Solution {
 					edges.put( p.getKey(), new Pair<>( n, 0L ) );
 					edges.put( n, new Pair<>( p.getKey(), 0L ) );
 				}
-			} else if ( Character.isLetter( p.getValue() ) ) {
-				addPortal( p, map, portals, src, dst );
-			}
-		}
-
-		for ( final var portal : portals.keySet() ) {
-			if ( portals.get( portal ).size() > 1 ) {
-				final Pair<Long, Long> a = portals.get( portal ).get( 0 );
-				final Pair<Long, Long> b = portals.get( portal ).get( 1 );
-				final Pair<Long, Long> in;
-				final Pair<Long, Long> out;
-				if ( a.getFirst() == 2 || a.getSecond() == 2 || a.getFirst() == 122 || a.getSecond() == 128 ) {// TODO find last two programmatically!
-					in = b;
-					out = a;
-				} else {
-					in = a;
-					out = b;
-				}
-				final long direction = first ? 0 : 1;
-				edges.put( in, new Pair<>( out, direction ) );
-				edges.put( out, new Pair<>( in, -direction ) );
+			} else if ( isLetter( p.getValue() ) ) {
+				//add portals
+				addPortal( p, map, portals, edges, src, dst, maxX, maxY, first );
 			}
 		}
 
 		return edges;
 	}
 
-	private void addPortal( final Map.Entry<Pair<Long, Long>, Character> c,
-			final Map<Pair<Long, Long>, Character> map,
-			final Multimap<String, Pair<Long, Long>> portals,
-			final Pair<Pair<Long, Long>, Long> src, final Pair<Pair<Long, Long>, Long> dst ) {
-		String portal = EMPTY;
-		final var pos = c.getKey();
+	private Map<Pair<Long, Long>, Character> initializeMap( final Stream<String> input ) {
+		final Map<Pair<Long, Long>, Character> map = new HashMap<>();
 
-		var n = new Pair<>( pos.getFirst() + 1, pos.getSecond() );
-		if ( isLetter( map, n ) ) {
-			portal = concat( c.getValue(), map.get( n ) );
-		}
+		final Pair<Long, Long> curr = new Pair<>( 0L, 0L );
+		input.forEach( line -> {
+			curr.setFirst( 0L );
+			for ( final Character c : line.toCharArray() ) {
+				map.put( new Pair<>( curr.getFirst(), curr.getSecond() ), c );
+				curr.setFirst( curr.getFirst() + 1 );
+			}
+			curr.setSecond( curr.getSecond() + 1 );
+		} );
+		return map;
+	}
 
-		n = new Pair<>( pos.getFirst() - 1, pos.getSecond() );
-		if ( isLetter( map, n ) ) {
-			portal = concat( map.get( n ), c.getValue() );
-		}
+	private void addPortal( final Map.Entry<Pair<Long, Long>, Character> p,
+			final Map<Pair<Long, Long>, Character> map, final Map<String, Pair<Long, Long>> portals,
+			final Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> edges,
+			final Pair<Pair<Long, Long>, Long> src, final Pair<Pair<Long, Long>, Long> dst,
+			final long maxX, final long maxY, final boolean first ) {
+		final Pair<Long, Long> pos = p.getKey();
+		final Character c = p.getValue();
+		final String portal = getPortalName( map, pos, c );
+		final var cells = computeNeighbours( pos, map );
 
-		n = new Pair<>( pos.getFirst(), pos.getSecond() + 1 );
-		if ( isLetter( map, n ) ) {
-			portal = concat( c.getValue(), map.get( n ) );
-		}
-
-		n = new Pair<>( pos.getFirst(), pos.getSecond() - 1 );
-		if ( isLetter( map, n ) ) {
-			portal = concat( map.get( n ), c.getValue() );
-		}
-
-		final var cells = computeNeighbours( c.getKey(), map );
-		if ( cells.size() == 1 ) {
+		if ( cells.size() > 0 ) {
 			final Pair<Long, Long> cell = cells.get( 0 );
-			portals.put( portal, cell );
-			if ( portal.equals( "AA" ) ) {
-				src.setFirst( new Pair<>( cell.getFirst(), cell.getSecond() ) );
+			if ( portals.containsKey( portal ) ) {
+				final Pair<Long, Long> in;
+				final Pair<Long, Long> out;
+				if ( isOutPortal( cell, maxX, maxY ) ) {
+					out = cell;
+					in = portals.get( portal );
+				} else {
+					out = portals.get( portal );
+					in = cell;
+				}
+				final long direction = first ? 0 : 1;
+				edges.put( in, new Pair<>( out, direction ) );
+				edges.put( out, new Pair<>( in, -direction ) );
+			} else if ( portal.equals( "AA" ) ) {
+				src.setFirst( cell );
 				src.setSecond( 0L ); //floor 0
 			} else if ( portal.equals( "ZZ" ) ) {
-				dst.setFirst( new Pair<>( cell.getFirst(), cell.getSecond() ) );
+				dst.setFirst( cell );
 				dst.setSecond( 0L ); //floor 0
+			} else {
+				portals.put( portal, cell );
 			}
 		}
 
+	}
+
+	private boolean isOutPortal( final Pair<Long, Long> cell, final long maxX, final long maxY ) {
+		final long x = cell.getFirst();
+		final long y = cell.getSecond();
+		return x == 2 || y == 2 || x == maxX - 2 || y == maxY - 2;
+	}
+
+	private String getPortalName( final Map<Pair<Long, Long>, Character> map,
+			final Pair<Long, Long> pos, final Character c ) {
+
+		var n = new Pair<>( pos );
+
+		n.setFirst( pos.getFirst() + 1 );
+		if ( isPortal( map, n ) ) {
+			return concat( c, map.get( n ) );
+		}
+
+		n.setFirst( pos.getFirst() - 1 );
+		if ( isPortal( map, n ) ) {
+			return concat( map.get( n ), c );
+		}
+
+		n.setFirst( pos.getFirst() );
+
+		n.setSecond( pos.getSecond() + 1 );
+		if ( isPortal( map, n ) ) {
+			return concat( c, map.get( n ) );
+		}
+
+		n.setSecond( pos.getSecond() - 1 );
+		if ( isPortal( map, n ) ) {
+			return concat( map.get( n ), c );
+		}
+
+		throw new IllegalStateException();
 	}
 
 	private String concat( final Character a, final Character b ) {
@@ -184,36 +199,40 @@ class AoC202019 implements Solution {
 			final Map<Pair<Long, Long>, Character> map ) {
 		final List<Pair<Long, Long>> neighbours = new ArrayList<>();
 		//add all the adjacent cells that can be reached
-		var n = new Pair<>( pos.getFirst() + 1, pos.getSecond() );
-		if ( isReachable( map, n ) ) {
-			neighbours.add( n );
+		var n = new Pair<>( pos );
+
+		n.setFirst( pos.getFirst() + 1 );
+		if ( isCell( map, n ) ) {
+			neighbours.add( new Pair<>( n ) );
 		}
 
-		n = new Pair<>( pos.getFirst() - 1, pos.getSecond() );
-		if ( isReachable( map, n ) ) {
-			neighbours.add( n );
+		n.setFirst( pos.getFirst() - 1 );
+		if ( isCell( map, n ) ) {
+			neighbours.add( new Pair<>( n ) );
 		}
 
-		n = new Pair<>( pos.getFirst(), pos.getSecond() + 1 );
-		if ( isReachable( map, n ) ) {
-			neighbours.add( n );
+		n.setFirst( pos.getFirst() );
+
+		n.setSecond( pos.getSecond() + 1 );
+		if ( isCell( map, n ) ) {
+			neighbours.add( new Pair<>( n ) );
 		}
 
-		n = new Pair<>( pos.getFirst(), pos.getSecond() - 1 );
-		if ( isReachable( map, n ) ) {
-			neighbours.add( n );
+		n.setSecond( pos.getSecond() - 1 );
+		if ( isCell( map, n ) ) {
+			neighbours.add( new Pair<>( n ) );
 		}
 
 		return neighbours;
 	}
 
-	private boolean isReachable( final Map<Pair<Long, Long>, Character> map,
+	private boolean isCell( final Map<Pair<Long, Long>, Character> map,
 			final Pair<Long, Long> n ) {
 		return map.getOrDefault( n, HASH ) == DOT;
 	}
 
-	private boolean isLetter( final Map<Pair<Long, Long>, Character> map,
+	private boolean isPortal( final Map<Pair<Long, Long>, Character> map,
 			final Pair<Long, Long> n ) {
-		return Character.isLetter( map.getOrDefault( n, HASH ) );
+		return isLetter( map.getOrDefault( n, HASH ) );
 	}
 }
