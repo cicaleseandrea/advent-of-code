42f0503c85963c411bca4bc6b764c65538daca09
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-23 12:19:47 +0000

    ♠️🃏🔀

---
+++ src/main/java/com/adventofcode/aoc2019/AoC222019.java
@@ -0,0 +1,20 @@
+package com.adventofcode.aoc2019;
+
+import java.util.stream.Stream;
+
+import com.adventofcode.Solution;
+
+class AoC222019 implements Solution {
+	public String solveFirstPart( final Stream<String> input ) {
+		return solve( input, true );
+	}
+
+	public String solveSecondPart( final Stream<String> input ) {
+		return solve( input, false );
+	}
+
+	private String solve( final Stream<String> input, final boolean first ) {
+
+		return null;
+	}
+}
