59fd81915db364354e386b9079d28745aa37d762
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-26 23:00:28 +0000

    🔌🕸🔀️

--- src/main/java/com/adventofcode/aoc2019/AoC232019.java
+++ src/main/java/com/adventofcode/aoc2019/AoC232019.java
@@ -36,84 +36,76 @@ class AoC232019 implements Solution {
 		final List<Long> program = toLongList( getFirstString( input ) );
 		final Map<Integer, BlockingQueue<Long>> in = new HashMap<>();
 		final List<BlockingQueue<Long>> out = new ArrayList<>();
-		final List<Computer2019> computers = new ArrayList<>();
 
 		for ( int i = 0; i < COMPUTERS; i++ ) {
 			//initialize computer
 			in.put( i, new LinkedBlockingQueue<>() );
 			out.add( new LinkedBlockingQueue<>() );
-			computers.add( new Computer2019( program, in.get( i ), out.get( i ) ) );
 			//start computer
 			in.get( i ).add( (long) i );
-			computers.get( i ).runAsync();
+			new Computer2019( program, in.get( i ), out.get( i ) ).runAsync();
 		}
 
 		//initialize NAT
 		in.put( NAT, new LinkedBlockingQueue<>() );
 
-		long res = runComputers( in, out, first );
-
-		return itoa( res );
+		return itoa( runComputers( in, out, first ) );
 	}
 
 	private long runComputers( final Map<Integer, BlockingQueue<Long>> receivers,
 			final List<BlockingQueue<Long>> senders, final boolean first ) {
-		try {
-			long lastYSent = 0L;
-			while ( true ) {
-				//always send a value to potentially unblock computers
-				IntStream.range( 0, COMPUTERS )
-						.parallel()
-						.forEach( to -> receivers.get( to ).add( -1L ) );
+		long lastYSent = 0L;
+		while ( true ) {
+			//always send a value to potentially unblock computers
+			IntStream.range( 0, COMPUTERS )
+					.parallel()
+					.forEach( to -> receivers.get( to ).add( -1L ) );
 
-				boolean idle = true;
-				for ( int from = 0; from < COMPUTERS; from++ ) {
-					final BlockingQueue<Long> sender = senders.get( from );
-					final Integer to = Optional.ofNullable( sender.poll() )
-							.map( Long::intValue )
-							.orElse( null );
-					if ( to != null ) {
-						idle = false; //a value has been sent
-						final BlockingQueue<Long> receiver = receivers.get( to );
-						final long x = sender.take();
-						final long y = sender.take();
-						if ( to == NAT ) {
-							if ( first ) {
-								return y;
-							} else {
-								receiver.clear(); //stores only latest packet
-							}
-						}
-						dispatchPacket( from, to, receiver, x, y );
+			boolean idle = true;
+			for ( int from = 0; from < COMPUTERS; from++ ) {
+				final BlockingQueue<Long> sender = senders.get( from );
+				final Integer to = Optional.ofNullable( sender.poll() )
+						.map( Long::intValue )
+						.orElse( null );
+				if ( to != null ) {
+					idle = false; //a value has been sent
+					if ( to == NAT ) {
+						receivers.get( to ).clear(); //stores only latest packet
 					}
-				}
-
-				//no value can be sent or received
-				if ( idle && isIdle( receivers, senders ) ) {
-					final BlockingQueue<Long> sender = receivers.get( NAT );
-					final BlockingQueue<Long> receiver = receivers.get( 0 );
-					final long x = sender.take();
-					final long y = sender.take();
-					dispatchPacket( NAT, 0, receiver, x, y );
-					if ( lastYSent == y ) {
+					final long y = dispatchPacket( from, to, receivers, sender );
+					if ( first && to == NAT ) {
 						return y;
-					} else {
-						lastYSent = y;
 					}
 				}
-
 			}
+
+			//no value can be sent or received
+			if ( !first && idle && isIdle( receivers, senders ) ) {
+				final BlockingQueue<Long> sender = receivers.get( NAT );
+				final long y = dispatchPacket( NAT, 0, receivers, sender );
+				if ( lastYSent == y ) {
+					return y;
+				}
+				lastYSent = y;
+			}
+
+		}
+	}
+
+	private long dispatchPacket( final int from, final int to,
+			final Map<Integer, BlockingQueue<Long>> receivers, final BlockingQueue<Long> sender ) {
+		try {
+			final BlockingQueue<Long> receiver = receivers.get( to );
+			final long x = sender.take();
+			final long y = sender.take();
+			receiver.add( x );
+			receiver.add( y );
+			printPacket( from, to, x, y );
+			return y;
 		} catch ( InterruptedException e ) {
 			e.printStackTrace();
 		}
-		return 0L;
-	}
-
-	private void dispatchPacket( final int from, final int to, final BlockingQueue<Long> receiver,
-			final long x, final long y ) {
-		receiver.add( x );
-		receiver.add( y );
-		printPacket( from, to, x, y );
+		throw new IllegalStateException();
 	}
 
 	private void printPacket( final int from, final int to, final long x, final long y ) {
