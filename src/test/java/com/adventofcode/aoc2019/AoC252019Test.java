e4bd03fde0657eb3278f9ef5b63c363606b90f6e
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-26 11:34:25 +0000

    🎄🎅🎁

---
+++ src/test/java/com/adventofcode/aoc2019/AoC252019Test.java
@@ -0,0 +1,31 @@
+package com.adventofcode.aoc2019;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+import static com.adventofcode.utils.Utils.MERRY_CHRISTMAS;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC252019Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC252019();
+
+    public AoC252019Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameterized.Parameters(name = PARAMETERS_MESSAGE)
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                { FIRST, getInput( INSTANCE ), "1109393410" },
+                { SECOND, getInput( INSTANCE ), MERRY_CHRISTMAS },
+        });
+    }
+}
