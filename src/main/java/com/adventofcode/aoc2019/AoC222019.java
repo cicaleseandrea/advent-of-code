94babb5b2bac1fe8b421fe118c696c7956dc7eb1
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-27 02:01:46 +0000

    ♠️🃏🔀

--- src/main/java/com/adventofcode/aoc2019/AoC222019.java
+++ src/main/java/com/adventofcode/aoc2019/AoC222019.java
@@ -2,116 +2,106 @@ package com.adventofcode.aoc2019;
 
 import static java.util.stream.Collectors.toList;
 
-import static com.adventofcode.utils.Utils.decrementMod;
 import static com.adventofcode.utils.Utils.extractIntegerFromString;
 import static com.adventofcode.utils.Utils.itoa;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
 
-//TODO
 class AoC222019 implements Solution {
 
-	private boolean PRINT = false;
+	private BigInteger deckSize;
+	private BigInteger positionResult;
+	private BigInteger head;
+	private BigInteger increment;
 
-	private long deckSize;
-	private long posRes;
-	private long posHead;
-	private boolean inverted;
-	private long steps;
+	private void deal( final BigInteger n ) {
+		positionResult = positionResult.multiply( n ).mod( deckSize );
 
-	private void deal( final long i ) {
-		posRes = ( posRes * i ) % deckSize;
-
-		//TODO?
-		steps = ( steps * -i );
-		print();
-		//steps=i then invert then cut -1
-		//		steps = i;
-		//		invert();
-		//		cut( -1 );
-		//deal 7
-		//0 3 6 9 2 5 8 1 4 7
-		//deal 9
-		//0 7 4 1 8 5 2 9 6 3
-		//my error
-		//0, 3, 6, 9, 2, 5, 8, 1, 4, 7,
-
-		//deal 9
-		//0, 9, 8, 7, 6, 5, 4, 3, 2, 1
-
+		//from here: https://www.reddit.com/r/adventofcode/comments/ee0rqi/2019_day_22_solutions/fbnkaju/
+		//after "deal n", the ith card becomes the i*nth card
+		//card at position 1 after "deal n", was at position 1/n before "deal n"
+		//using our two variables to describe its position, that card moved
+		//from:
+		//head + old_increment/n
+		//to:
+		//head + new_increment
+		//which means that increment changed like this:
+		//increment = (increment/n) % size
+		increment = increment.multiply( n.modInverse( deckSize ) );
 	}
 
-	private void cut( final long i ) {
-		posRes = decrementMod( posRes, i, deckSize );
-		//0, 3, 6, 9, 2, 5, 8, 1, 4, 7,
-		//9, 2, 5, 8, 1, 4, 7, 0, 3, 6,
-		if ( !inverted ) {
-			posHead = decrementMod( posHead, -i * steps, deckSize );
-		} else {
-			posHead = decrementMod( posHead, i * steps, deckSize );
-		}
-		print();
+	private void cut( final BigInteger n ) {
+		positionResult = positionResult.subtract( n ).mod( deckSize );
+
+		head = head.add( increment.multiply( n ) ).mod( deckSize );
 	}
 
 	private void invert() {
-		posRes = decrementMod( -posRes, 1, deckSize );
-		//0, 3, 6, 9, 2, 5, 8, 1, 4, 7,
+		positionResult = positionResult.add( BigInteger.ONE ).negate().mod( deckSize );
 
-		inverted = !inverted;
-		if ( !inverted ) {
-			posHead = decrementMod( posHead, -steps, deckSize );
-		} else {
-			posHead = decrementMod( posHead, steps, deckSize );
-		}
-		print();
+		increment = increment.negate();
+		head = head.add( increment ).mod( deckSize );
 	}
 
-	private void print() {
-		if ( PRINT ) {
-			long e = posHead;
-			System.out.println( "RES: " + posRes );
-			System.out.println( "steps: " + steps + " inverted: " + inverted );
-			for ( int i = 0; i < deckSize; i++ ) {
-				System.out.print( e + ", " );
-				if ( !inverted ) {
-					e = decrementMod( e, -steps, deckSize );
-				} else {
-					e = decrementMod( e, steps, deckSize );
-				}
-			}
-			System.out.println();
-		}
+	private BigInteger getCardAtPosition( final BigInteger head, final BigInteger increment,
+			final BigInteger position ) {
+		return head.add( increment.multiply( position ) ).mod( deckSize );
 	}
 
 	public String solveFirstPart( final Stream<String> input ) {
-		return solve( input, true );
+		return solve( input, true, () -> itoa( positionResult ) );
 	}
 
 	public String solveSecondPart( final Stream<String> input ) {
-		return solve( input, false );
+		return solve( input, false, () -> {
+			//starting from increment and head that are the result of one shuffle
+			//calculate increment and head as a result of many shuffles
+			final BigInteger shuffles = BigInteger.valueOf( 101741582076661L );
+
+			//after each shuffle, increment is just multiplied by some constant number
+			//incrementShuffles = increment * increment * increment ... * increment (shuffles times)
+			final BigInteger incrementShuffles = increment.modPow( shuffles, deckSize );
+
+			//from here: https://www.reddit.com/r/adventofcode/comments/ee0rqi/2019_day_22_solutions/fbnkaju/
+			//after each shuffle, head is incremented by some constant multiple of the current increment
+			//headShuffles = head*(increment before any shuffle) + head*(increment after 1 shuffle) + head*(increment after 2 shuffles) + ... + head*(increment after shuffles-1 shuffles)
+			//headShuffles = head*1 + head*increment + head*increment^2 + ... + head*increment^(shuffles-1)
+			//headShuffles = head * (1 + increment + increment^2 + ... + increment^(shuffles-1))
+			//using geometric series:
+			//headShuffles = head * (1 - increment^shuffles) / (1 - increment)
+			final BigInteger headShuffles = head.multiply(
+					BigInteger.ONE.subtract( incrementShuffles ) )
+					.multiply( BigInteger.ONE.subtract( increment ).modInverse( deckSize ) )
+					.mod( deckSize );
+			return getCardAtPosition( headShuffles, incrementShuffles,
+					BigInteger.valueOf( 2020 ) ).toString();
+		} );
 	}
 
-	private String solve( final Stream<String> input, final boolean first ) {
+	private String solve( final Stream<String> input, final boolean first,
+			final Supplier<String> computeResult ) {
 		final List<String> lines = input.collect( toList() );
-		deckSize = lines.size() < 20 ? 10 : first ? 10007 : 119315717514047L;
-		posRes = lines.size() < 20 ? 9 : 2019;
-		posHead = 0;
-		inverted = false;
-		steps = 1;
+		deckSize = lines.size() < 20 ? BigInteger.TEN : first ? BigInteger.valueOf(
+				10007 ) : BigInteger.valueOf( 119315717514047L );
+		positionResult = lines.size() < 20 ? BigInteger.valueOf( 9 ) : BigInteger.valueOf( 2019 );
+		head = BigInteger.ZERO;
+		increment = BigInteger.ONE;
 		for ( final String line : lines ) {
 			if ( line.startsWith( "cut" ) ) {
-				cut( extractIntegerFromString( line ) );
+				cut( BigInteger.valueOf( extractIntegerFromString( line ) ) );
 			} else if ( line.startsWith( "deal into" ) ) {
 				invert();
 			} else {
-				deal( extractIntegerFromString( line ) );
+				deal( BigInteger.valueOf( extractIntegerFromString( line ) ) );
 			}
 		}
 
-		return itoa( posRes );
+		return computeResult.get();
 	}
 
 }
