e0daeed10ca6735485b3e0c1eee03e22a7796453
cicaleseandrea <andrea.cicalese@fanduel.com>
2019-12-28 02:57:02 +0000

    02 2015

---
+++ src/test/java/com/adventofcode/aoc2015/AoC022015Test.java
@@ -0,0 +1,35 @@
+package com.adventofcode.aoc2015;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC022015Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC022015();
+
+    public AoC022015Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameters(name = "{0}({index})->{2}")
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                {FIRST, "2x3x4", "58"},
+                {FIRST, "1x1x10", "43"},
+                {FIRST, getInput(INSTANCE), "1598415"},
+                {SECOND, "2x3x4", "34"},
+                {SECOND, "1x1x10", "14"},
+                {SECOND, getInput(INSTANCE), "3812909"},
+        });
+    }
+}
