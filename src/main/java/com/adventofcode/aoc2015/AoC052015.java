4bf6addb5167adf7a8471c414053359461a70c01
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-01-24 16:31:01 +0000

    5 2015

---
+++ src/main/java/com/adventofcode/aoc2015/AoC052015.java
@@ -0,0 +1,46 @@
+package com.adventofcode.aoc2015;
+
+import static com.adventofcode.utils.Utils.itoa;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import com.adventofcode.Solution;
+
+class AoC052015 implements Solution {
+
+	@Override
+	public String solveFirstPart( final Stream<String> input ) {
+		return solve( input, true );
+	}
+
+	@Override
+	public String solveSecondPart( final Stream<String> input ) {
+		return solve( input, false );
+	}
+
+	private String solve( final Stream<String> input, final boolean first ) {
+		final Pattern pattern;
+		if ( first ) {
+			//@formatter:off
+			pattern = Pattern.compile(
+					"(?!.*ab|.*cd|.*pq|.*xy)" //does not contain these substrings
+					+ "(?=.*[aeiou].*[aeiou].*[aeiou])" //at least 3 vowels
+					+ "(?=.*([\\w])\\1)" //at least one double letter
+					+ ".*"// whatever
+			);
+			//@formatter:on
+		} else {
+			//@formatter:off
+			pattern = Pattern.compile(
+					"(?=.*([\\w][\\w]).*\\1)" //pair of letters that appears at least twice
+					+ "(?=.*([\\w])[\\w]\\2)" //one letter which repeats with exactly one letter between them
+					+ ".*" //whatever
+			);
+			//@formatter:on
+		}
+		return itoa( input.map( pattern::matcher ).filter( Matcher::matches ).count() );
+	}
+
+}
---
+++ src/test/java/com/adventofcode/aoc2015/AoC052015Test.java
@@ -0,0 +1,40 @@
+package com.adventofcode.aoc2015;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC052015Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC052015();
+
+    public AoC052015Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameters(name = "{0}({index})->{2}")
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                {FIRST, "ugknbfddgicrmopn", "1"},
+                {FIRST, "aaa", "1"},
+                {FIRST, "jchzalrnumimnmhp", "0"},
+                {FIRST, "haegwjzuvuyypxyu", "0"},
+                {FIRST, "dvszwmarrgswjxmb", "0"},
+                {FIRST, getInput(INSTANCE), "238"},
+                {SECOND, "qjhvhtzxzqqjkmpb", "1"},
+                {SECOND, "xxyxx", "1"},
+                {SECOND, "uurcxstgmygtbstg", "0"},
+                {SECOND, "ieodomkazucvgmuy", "0"},
+                {SECOND, getInput(INSTANCE), "69"},
+        });
+    }
+}
