d206dc845b0ddca98fe39491c89b9e3b7fbd2ff4
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-12-03 12:32:32 +0000

    day 3 2020

---
+++ src/test/java/com/adventofcode/aoc2020/AoC032020Test.java
@@ -0,0 +1,56 @@
+package com.adventofcode.aoc2020;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC032020Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC032020();
+
+    public AoC032020Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameterized.Parameters(name = PARAMETERS_MESSAGE)
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                { FIRST, """
+                        ..##.......
+                        #...#...#..
+                        .#....#..#.
+                        ..#.#...#.#
+                        .#...##..#.
+                        ..#.##.....
+                        .#.#.#....#
+                        .#........#
+                        #.##...#...
+                        #...##....#
+                        .#..#...#.#
+                        """, "7" },
+                { FIRST, getInput( INSTANCE ), "211" },
+                { SECOND, """
+                        ..##.......
+                        #...#...#..
+                        .#....#..#.
+                        ..#.#...#.#
+                        .#...##..#.
+                        ..#.##.....
+                        .#.#.#....#
+                        .#........#
+                        #.##...#...
+                        #...##....#
+                        .#..#...#.#
+                        """, "336" },
+                { SECOND, getInput( INSTANCE ), "3584591857" }
+        });
+    }
+}
