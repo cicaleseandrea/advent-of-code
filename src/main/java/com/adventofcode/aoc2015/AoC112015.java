d9c0f4d1a6b8a9a9b13e459cf39dcb59c2b6e84f
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-12-07 20:17:59 +0000

    10 2015

--- src/main/java/com/adventofcode/aoc2015/AoC112015.java
+++ src/main/java/com/adventofcode/aoc2015/AoC112015.java
@@ -5,10 +5,12 @@ import static com.adventofcode.utils.Utils.itoa;
 
 import java.util.stream.Stream;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.adventofcode.Solution;
 import com.adventofcode.utils.Utils;
 
-class AoC102015 implements Solution {
+class AoC112015 implements Solution {
 
 	@Override
 	public String solveFirstPart( final Stream<String> input ) {
@@ -20,6 +22,7 @@ class AoC102015 implements Solution {
 		return solve( input, 50 );
 	}
 
+	@NotNull
 	private String solve( final Stream<String> input, final int iterations ) {
 		StringBuilder sequence = new StringBuilder( Utils.getFirstString( input ) );
 		for ( int i = 0; i < ( sequence.length() < 10 ? 1 : iterations ); i++ ) {
---
+++ src/test/java/com/adventofcode/aoc2015/AoC112015Test.java
@@ -0,0 +1,33 @@
+package com.adventofcode.aoc2015;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC112015Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC112015();
+
+    public AoC112015Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameters(name = "{0}({index})->{2}")
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                {FIRST, "111221", "0"},
+                {FIRST, getInput(INSTANCE), "0"},
+                {SECOND, "111221", "0"},
+                {SECOND, getInput(INSTANCE), "0"},
+        });
+    }
+}
