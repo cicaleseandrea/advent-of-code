d3144a95096477fde48e2ccf5efcb6b2791f4146
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-12-09 19:55:01 +0000

    14 2015

---
+++ src/main/java/com/adventofcode/aoc2015/AoC142015.java
@@ -0,0 +1,76 @@
+package com.adventofcode.aoc2015;
+
+import static java.util.stream.Collectors.toList;
+
+import static com.adventofcode.utils.Utils.atoi;
+import static com.adventofcode.utils.Utils.itoa;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.adventofcode.Solution;
+import com.adventofcode.utils.Triplet;
+
+class AoC142015 implements Solution {
+	private static final Pattern SPEED_REGEX = Pattern.compile(
+			"\\w+ can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds." );
+
+	@Override
+	public String solveFirstPart( final Stream<String> input ) {
+		return solve( input, true );
+	}
+
+	@Override
+	public String solveSecondPart( final Stream<String> input ) {
+		return solve( input, false );
+	}
+
+	@NotNull
+	private String solve( final Stream<String> input, final boolean first ) {
+		final var reindeer = input.map( line -> {
+			final var matcher = SPEED_REGEX.matcher( line );
+			if ( !matcher.matches() ) {
+				throw new IllegalArgumentException();
+			}
+			final int speed = atoi( matcher.group( 1 ) );
+			final int runTime = atoi( matcher.group( 2 ) );
+			final int restTime = atoi( matcher.group( 3 ) );
+			return new Triplet<>( speed, runTime, restTime );
+		} ).collect( toList() );
+
+		final int seconds = reindeer.size() < 3 ? 1000 : 2503;
+		if ( first ) {
+			return itoa( Collections.max( computeRun( reindeer, seconds ) ) );
+		}
+
+		final int[] finalScores = new int[reindeer.size()];
+		for ( int i = 1; i <= seconds; i++ ) {
+			final List<Integer> tmpScores = computeRun( reindeer, i );
+			final int max = Collections.max( tmpScores );
+			IntStream.range( 0, tmpScores.size() )
+					.filter( r -> max == tmpScores.get( r ) )
+					.forEach( winner -> finalScores[winner]++ );
+		}
+		return itoa( Arrays.stream( finalScores ).max().orElseThrow() );
+	}
+
+	private List<Integer> computeRun( final List<Triplet<Integer, Integer, Integer>> reindeer,
+			final int i ) {
+		return reindeer.stream()
+				.map( r -> computeScore( i, r.getFirst(), r.getSecond(), r.getThird() ) )
+				.collect( toList() );
+	}
+
+	private int computeScore( final int seconds, final int speed, final int runTime,
+			final int restTime ) {
+		final int time = runTime + restTime;
+		return seconds / time * runTime * speed + Math.min( seconds % time, runTime ) * speed;
+	}
+
+}
---
+++ src/test/java/com/adventofcode/aoc2015/AoC142015Test.java
@@ -0,0 +1,39 @@
+package com.adventofcode.aoc2015;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC142015Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC142015();
+
+    public AoC142015Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameters(name = "{0}({index})->{2}")
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                {FIRST, """
+                        Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+                        Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+                        """, "1120"},
+                {FIRST, getInput(INSTANCE), "2696"},
+                {SECOND, """
+                        Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+                        Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+                        """, "689"},
+                {SECOND, getInput(INSTANCE), "1084"},
+        });
+    }
+}
