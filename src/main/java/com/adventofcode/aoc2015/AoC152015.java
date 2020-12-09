490be158974db9aa0507220f8ed673ce1001212d
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-12-09 19:58:46 +0000

    15 2015

---
+++ src/main/java/com/adventofcode/aoc2015/AoC152015.java
@@ -0,0 +1,23 @@
+package com.adventofcode.aoc2015;
+
+import static com.adventofcode.utils.Utils.itoa;
+
+import java.util.stream.Stream;
+
+import com.adventofcode.Solution;
+
+class AoC152015 implements Solution {
+
+	@Override
+	public String solveFirstPart( final Stream<String> input ) {
+
+		return itoa( 0 );
+	}
+
+	@Override
+	public String solveSecondPart( final Stream<String> input ) {
+
+		return itoa( 0 );
+	}
+
+}
