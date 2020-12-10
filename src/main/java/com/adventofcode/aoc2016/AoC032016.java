03cbf8320f700fd95fc3c5025e24a654334205f7
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-12-10 23:59:38 +0000

    3 2016

---
+++ src/main/java/com/adventofcode/aoc2016/AoC032016.java
@@ -0,0 +1,42 @@
+package com.adventofcode.aoc2016;
+
+import static java.util.stream.Collectors.toList;
+
+import static com.adventofcode.utils.Utils.itoa;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import com.adventofcode.Solution;
+import com.adventofcode.utils.Utils;
+
+class AoC032016 implements Solution {
+
+	@Override
+	public String solveFirstPart( final Stream<String> input ) {
+		return itoa( input.map( Utils::toLongList ).filter( AoC032016::isTriangle ).count() );
+	}
+
+	@Override
+	public String solveSecondPart( final Stream<String> input ) {
+		final var lines = input.collect( toList() );
+		long count = IntStream.iterate( 0, i -> i < lines.size(), i -> i + 3 )
+				.boxed()
+				.flatMap( i -> IntStream.range( 0, 3 )
+						.mapToObj( j -> List.of( Utils.toLongList( lines.get( i ) ).get( j ),
+								Utils.toLongList( lines.get( i + 1 ) ).get( j ),
+								Utils.toLongList( lines.get( i + 2 ) ).get( j ) ) ) )
+				.filter( AoC032016::isTriangle )
+				.count();
+		return itoa( count );
+	}
+
+	private static boolean isTriangle( final List<Long> numbers ) {
+		final long x = numbers.get( 0 );
+		final long y = numbers.get( 1 );
+		final long z = numbers.get( 2 );
+		return x + y > z && y + z > x && x + z > y;
+	}
+
+}
---
+++ src/test/java/com/adventofcode/aoc2016/AoC032016Test.java
@@ -0,0 +1,32 @@
+package com.adventofcode.aoc2016;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC032016Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC032016();
+
+    public AoC032016Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameters(name = "{0}({index})->{2}")
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                { FIRST, "5 10 25", "0" },
+                { FIRST, getInput( INSTANCE ), "983" },
+                { SECOND, getInput( INSTANCE ), "1836" },
+        });
+    }
+}
