b6fab495433c690e3e252e407e2eca0b2ec9d7f0
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-12-10 12:58:06 +0000

    day 10 2020

--- src/main/java/com/adventofcode/aoc2020/AoC102020.java
+++ src/main/java/com/adventofcode/aoc2020/AoC102020.java
@@ -3,8 +3,7 @@ package com.adventofcode.aoc2020;
 import static com.adventofcode.utils.Utils.itoa;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
@@ -14,22 +13,56 @@ class AoC102020 implements Solution {
 
 	@Override
 	public String solveFirstPart( final Stream<String> input ) {
-		final var list = Utils.toLongList( input );
-		Collections.sort( list );
-		list.add( 0, 0L );
-		list.add( list.get( list.size() - 1 ) + 3 );
-		final Map<Long, Long> diffs = new HashMap<>();
-		for ( int i = 1; i < list.size(); i++ ) {
-			final long diff = list.get( i ) - list.get( i - 1 );
-			diffs.merge( diff, 1L, Long::sum );
-		}
-		return itoa( diffs.get( 1L ) * diffs.get( 3L ) );
+		return solve( input, true );
 	}
 
 	@Override
 	public String solveSecondPart( final Stream<String> input ) {
+		return solve( input, false );
+	}
 
-		return itoa( 0 );
+	private String solve( final Stream<String> input, final boolean first ) {
+		final var list = initializeList( input );
+		long ones = 0;
+		long threes = 0;
+		int windowLength = 0;
+		long totalArrangements = 1L;
+		for ( int i = 1; i < list.size(); i++ ) {
+			windowLength++;
+			final long diff = list.get( i ) - list.get( i - 1 );
+			switch ( (int) diff ) {
+			case 1 -> ones++;
+			case 3 -> {
+				threes++;
+				totalArrangements *= computeArrangements( windowLength );
+				windowLength = 0;
+			}
+			default -> throw new IllegalStateException( "Unexpected value: " + diff );
+			}
+		}
+		if ( first ) {
+			return itoa( ones * threes );
+		} else {
+			return itoa( totalArrangements );
+		}
+	}
+
+	private double computeArrangements( final int windowLength ) {
+		return switch ( windowLength ) {
+			case 1, 2 -> 1;
+			case 3 -> 2;
+			case 4 -> 4;
+			case 5 -> 7;
+			default -> throw new IllegalStateException( "Unexpected value: " + windowLength );
+		};
+	}
+
+	private List<Long> initializeList( final Stream<String> input ) {
+		final var list = Utils.toLongList( input );
+		Collections.sort( list );
+		list.add( 0, 0L );
+		list.add( list.get( list.size() - 1 ) + 3 );
+		return list;
 	}
 
 }
