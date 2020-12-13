166fbb5bcdfe8955125171d16c9d1581fdae07df
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-12-13 21:25:43 +0000

    day 13 2020

---
+++ src/test/java/com/adventofcode/aoc2020/AoC132020Test.java
@@ -0,0 +1,59 @@
+package com.adventofcode.aoc2020;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC132020Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC132020();
+
+    public AoC132020Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameters(name = PARAMETERS_MESSAGE)
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                { FIRST, """
+                        939
+                        7,13,x,x,59,x,31,19
+                        """, "295"  },
+                { FIRST, getInput( INSTANCE ), "3882" },
+                { SECOND, """
+                        939
+                        17,x,13,19
+                        """, "3417"  },
+                { SECOND, """
+                        939
+                        7,13,x,x,59,x,31,19
+                        """, "1068781"  },
+                { SECOND, """
+                        939
+                        67,7,59,61
+                        """, "754018"  },
+                { SECOND, """
+                        939
+                        67,x,7,59,61
+                        """, "779210"  },
+                { SECOND, """
+                        939
+                        67,7,x,59,61
+                        """, "1261476"  },
+                { SECOND, """
+                        939
+                        1789,37,47,1889
+                        """, "1202161486"  },
+                { SECOND, getInput( INSTANCE ), "867295486378319" }
+        });
+    }
+}
