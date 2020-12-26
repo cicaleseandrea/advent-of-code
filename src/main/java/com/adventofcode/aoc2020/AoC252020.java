db372f6707653389c9e6c6b87d6ac501d9a0a864
cicaleseandrea <andrea.cicalese@fanduel.com>
2020-12-26 11:24:21 +0000

    day 25 2020

--- src/main/java/com/adventofcode/aoc2020/AoC252020.java
+++ src/main/java/com/adventofcode/aoc2020/AoC252020.java
@@ -1,14 +1,16 @@
 package com.adventofcode.aoc2020;
 
 import static com.adventofcode.utils.Utils.MERRY_CHRISTMAS;
+import static com.adventofcode.utils.Utils.itoa;
 import static com.adventofcode.utils.Utils.toLongList;
 
-import java.math.BigInteger;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
 
 class AoC252020 implements Solution {
+
+	private static final int MOD = 20201227;
 
 	@Override
 	public String solveFirstPart( final Stream<String> input ) {
@@ -16,15 +18,12 @@ class AoC252020 implements Solution {
 		final long cardPublicKey = numbers.get( 0 );
 		final long doorPublicKey = numbers.get( 1 );
 		long value = 1;
-		int cardLoopSize = 0;
+		long encryptionKey = 1;
 		while ( value != cardPublicKey ) {
-			value = ( value * 7 ) % 20201227;
-			cardLoopSize++;
+			value = ( value * 7 ) % MOD;
+			encryptionKey = ( encryptionKey * doorPublicKey ) % MOD;
 		}
-
-		return BigInteger.valueOf( doorPublicKey )
-				.modPow( BigInteger.valueOf( cardLoopSize ), BigInteger.valueOf( 20201227 ) )
-				.toString();
+		return itoa( encryptionKey );
 	}
 
 	@Override
