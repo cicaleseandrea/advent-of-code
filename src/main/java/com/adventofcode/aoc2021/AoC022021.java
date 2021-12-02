1de4ce127121d496a2f99da2b896aac513acd4e3
cicaleseandrea <andrea.cicalese@fanduel.com>
2021-12-02 13:11:10 +0000

    refactor day 2 2021

--- src/main/java/com/adventofcode/aoc2021/AoC022021.java
+++ src/main/java/com/adventofcode/aoc2021/AoC022021.java
@@ -3,6 +3,7 @@ package com.adventofcode.aoc2021;
 import static com.adventofcode.utils.Utils.charToInt;
 import static com.adventofcode.utils.Utils.itoa;
 
+import java.util.function.LongBinaryOperator;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
@@ -11,23 +12,16 @@ class AoC022021 implements Solution {
 
 	@Override
 	public String solveFirstPart( final Stream<String> input ) {
-		long position = 0;
-		long depth = 0;
-		for ( final String command : input.toList() ) {
-			final int digit = charToInt( command.charAt( command.length() - 1 ) );
-			if ( command.startsWith( "forward" ) ) {
-				position += digit;
-			} else if ( command.startsWith( "down" ) ) {
-				depth += digit;
-			} else if ( command.startsWith( "up" ) ) {
-				depth -= digit;
-			}
-		}
-		return itoa( position * depth );
+		return solve( input, ( digit, aim ) -> 0, ( digit, aim ) -> digit );
 	}
 
 	@Override
 	public String solveSecondPart( final Stream<String> input ) {
+		return solve( input, ( digit, aim ) -> digit * aim, ( digit, aim ) -> 0 );
+	}
+
+	private String solve( final Stream<String> input, final LongBinaryOperator computeDepthForward,
+			final LongBinaryOperator computeDepthUpDown ) {
 		long position = 0;
 		long depth = 0;
 		long aim = 0;
@@ -35,11 +29,13 @@ class AoC022021 implements Solution {
 			final long digit = charToInt( command.charAt( command.length() - 1 ) );
 			if ( command.startsWith( "forward" ) ) {
 				position += digit;
-				depth += aim * digit;
+				depth += computeDepthForward.applyAsLong( digit, aim );
 			} else if ( command.startsWith( "down" ) ) {
 				aim += digit;
+				depth += computeDepthUpDown.applyAsLong( digit, aim );
 			} else if ( command.startsWith( "up" ) ) {
 				aim -= digit;
+				depth -= computeDepthUpDown.applyAsLong( digit, aim );
 			}
 		}
 		return itoa( position * depth );
