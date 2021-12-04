25c4fdcfd9c4be3944d58227a6aa1e83fcb73af4
cicaleseandrea <andrea.cicalese@fanduel.com>
2021-12-04 16:32:12 +0000

    refactor day 3 2021

--- src/main/java/com/adventofcode/aoc2021/AoC032021.java
+++ src/main/java/com/adventofcode/aoc2021/AoC032021.java
@@ -1,6 +1,5 @@
 package com.adventofcode.aoc2021;
 
-import static com.adventofcode.utils.Utils.charToInt;
 import static com.adventofcode.utils.Utils.itoa;
 
 import java.util.HashSet;
@@ -10,58 +9,64 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
+import com.adventofcode.utils.Pair;
 
 class AoC032021 implements Solution {
 
 	@Override
 	public String solveFirstPart( final Stream<String> input ) {
-		final var numbers = input.toList();
-		int gammaRate = 0;
-		final int numberLength = numbers.get( 0 ).length();
-		for ( int i = 0; i < numberLength; i++ ) {
-			int ones = 0;
-			for ( final String number : numbers ) {
-				final int bit = charToInt( number.charAt( i ) );
-				ones += ( bit == 1 ) ? +1 : -1;
-			}
-			gammaRate *= 2;
-			gammaRate += ( ones > 0 ) ? 1 : 0;
-		}
-		final int episolonRate = gammaRate ^ ( ( 1 << numberLength ) - 1 );
-		return itoa( episolonRate * gammaRate );
+		final Set<String> numbers = input.collect( Collectors.toSet() );
+		final long gammaRate = solve( ( ones, zeroes ) -> ones >= zeroes, numbers, 0, true );
+		final long epsilonRate = solve( ( ones, zeroes ) -> ones < zeroes, numbers, 0, true );
+		return itoa( gammaRate * epsilonRate );
 	}
 
 	@Override
 	public String solveSecondPart( final Stream<String> input ) {
 		final Set<String> numbers = input.collect( Collectors.toSet() );
-		final long oxygen = solve( ( a, b ) -> a >= b, numbers, 0 );
-		final long co2 = solve( ( a, b ) -> a < b, numbers, 0 );
+		final long oxygen = solve( ( ones, zeroes ) -> ones >= zeroes, numbers, 0, false );
+		final long co2 = solve( ( ones, zeroes ) -> ones < zeroes, numbers, 0, false );
 		return itoa( oxygen * co2 );
 	}
 
-	private int solve( final BiPredicate<Integer, Integer> predicate, final Set<String> numbers,
+	private int solve( final BiPredicate<Integer, Integer> bitCriteria, final Set<String> numbers,
+			final int position, final boolean first ) {
+		if ( numbers.size() == 1 ) {
+			return numbers.stream().map( n -> Integer.parseInt( n, 2 ) ).findFirst().orElseThrow();
+		}
+
+		final int numberLength = numbers.stream().findFirst().orElseThrow().length();
+		if ( position >= numberLength ) {
+			return 0;
+		}
+
+		final var onesAndZeroes = splitOnesAndZeroes( numbers, position );
+		final var ones = onesAndZeroes.getFirst();
+		final var zeroes = onesAndZeroes.getSecond();
+		final boolean bitCriteriaResult = bitCriteria.test( ones.size(), zeroes.size() );
+
+		if ( first ) {
+			final int head = bitCriteriaResult ? ( 1 << ( numberLength - position - 1 ) ) : 0;
+			final int tail = solve( bitCriteria, numbers, position + 1, first );
+			return head + tail;
+		} else {
+			final Set<String> keep = bitCriteriaResult ? ones : zeroes;
+			return solve( bitCriteria, keep, position + 1, first );
+		}
+	}
+
+	private Pair<HashSet<String>, HashSet<String>> splitOnesAndZeroes( final Set<String> numbers,
 			final int position ) {
-		var ones = new HashSet<String>();
-		var zeroes = new HashSet<String>();
+		final var ones = new HashSet<String>();
+		final var zeroes = new HashSet<String>();
 		for ( final String number : numbers ) {
-			final int bit = charToInt( number.charAt( position ) );
-			if ( bit == 1 ) {
+			final char bit = number.charAt( position );
+			if ( bit == '1' ) {
 				ones.add( number );
 			} else {
 				zeroes.add( number );
 			}
 		}
-
-		final Set<String> keep;
-		if ( predicate.test( ones.size(), zeroes.size() ) ) {
-			keep = ones;
-		} else {
-			keep = zeroes;
-		}
-		if ( keep.size() == 1 ) {
-			return keep.stream().map( n -> Integer.parseInt( n, 2 ) ).findFirst().orElseThrow();
-		} else {
-			return solve( predicate, keep, position + 1 );
-		}
+		return new Pair<>( ones, zeroes );
 	}
 }
