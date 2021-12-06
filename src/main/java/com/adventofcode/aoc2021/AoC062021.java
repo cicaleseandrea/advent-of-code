2434fd93855b5b505c0682a88f42207ad1c00ce9
cicaleseandrea <andrea.cicalese@fanduel.com>
2021-12-06 18:06:21 +0000

    refactor day 6 2021

--- src/main/java/com/adventofcode/aoc2021/AoC062021.java
+++ src/main/java/com/adventofcode/aoc2021/AoC062021.java
@@ -3,17 +3,12 @@ package com.adventofcode.aoc2021;
 import static com.adventofcode.utils.Utils.itoa;
 import static com.adventofcode.utils.Utils.toLongStream;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
-import com.adventofcode.utils.Pair;
 
 class AoC062021 implements Solution {
-
-	// used for memoization
-	private static final Map<Pair<Long, Long>, Long> cache = new HashMap<>();
 
 	@Override
 	public String solveFirstPart( final Stream<String> input ) {
@@ -26,30 +21,18 @@ class AoC062021 implements Solution {
 	}
 
 	private String solve( final Stream<String> input, final int days ) {
-		final var ages = toLongStream( input.findFirst().orElseThrow() );
-		final long numberOfFish = ages.mapToLong( age -> solve( age, days ) ).sum();
+		final long[] ages = new long[9];
+		toLongStream( input.findFirst().orElseThrow() ).forEach( age -> ages[age.intValue()]++ );
 
-		return itoa( numberOfFish );
-	}
-
-	private long solve( final long age, final long days ) {
-		final Pair<Long, Long> input = new Pair<>( age, days );
-		if ( cache.containsKey( input ) ) {
-			return cache.get( input );
+		for ( int i = 0; i < days; i++ ) {
+			final long spawn = ages[0];
+			for ( int j = 0; j < 8; j++ ) {
+				// age decreases
+				ages[j] = ages[j + 1];
+			}
+			ages[6] += spawn;
+			ages[8] = spawn;
 		}
-
-		final long daysAfterSpawn = days - ( age + 1 );
-		final long result;
-		if ( daysAfterSpawn < 0 ) {
-			// no time left to spawn
-			result = 1;
-		} else {
-			// spawn
-			result = /*existing one*/ solve( 6, daysAfterSpawn ) + /*new one*/ solve( 8,
-					daysAfterSpawn );
-		}
-
-		cache.put( input, result );
-		return result;
+		return itoa( Arrays.stream( ages ).sum() );
 	}
 }
