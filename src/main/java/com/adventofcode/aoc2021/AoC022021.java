236793a735e7314393977bd56026317ee078a2c4
cicaleseandrea <andrea.cicalese@fanduel.com>
2021-12-14 19:38:58 +0000

    refactor day 2 2021

--- src/main/java/com/adventofcode/aoc2021/AoC022021.java
+++ src/main/java/com/adventofcode/aoc2021/AoC022021.java
@@ -1,14 +1,17 @@
 package com.adventofcode.aoc2021;
 
-import static com.adventofcode.utils.Utils.charToInt;
+import static com.adventofcode.utils.Utils.atoi;
 import static com.adventofcode.utils.Utils.itoa;
 
 import java.util.function.LongBinaryOperator;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import com.adventofcode.Solution;
 
 class AoC022021 implements Solution {
+
+	private static final Pattern COMMAND_REGEX = Pattern.compile( "(\\D+) (\\d)" );
 
 	@Override
 	public String solveFirstPart( final Stream<String> input ) {
@@ -25,17 +28,27 @@ class AoC022021 implements Solution {
 		long position = 0;
 		long depth = 0;
 		long aim = 0;
-		for ( final String command : input.toList() ) {
-			final long digit = charToInt( command.charAt( command.length() - 1 ) );
-			if ( command.startsWith( "forward" ) ) {
+		for ( final String line : input.toList() ) {
+			final var matcher = COMMAND_REGEX.matcher( line );
+			if ( !matcher.matches() ) {
+				throw new IllegalArgumentException();
+			}
+			final var command = matcher.group( 1 );
+			final long digit = atoi( matcher.group( 2 ) );
+			switch ( command ) {
+			case "forward" -> {
 				position += digit;
 				depth += computeDepthForward.applyAsLong( digit, aim );
-			} else if ( command.startsWith( "down" ) ) {
+			}
+			case "down" -> {
 				aim += digit;
 				depth += computeDepthUpDown.applyAsLong( digit, aim );
-			} else if ( command.startsWith( "up" ) ) {
+			}
+			case "up" -> {
 				aim -= digit;
 				depth -= computeDepthUpDown.applyAsLong( digit, aim );
+			}
+			default -> throw new IllegalStateException( "Unexpected value: " + command );
 			}
 		}
 		return itoa( position * depth );
