8332b09a330d0893a9666c357c9f7d7d03cd5050
cicaleseandrea <andrea.cicalese@fanduel.com>
2021-12-24 16:39:42 +0100

    day 23 2021 (solved manually)

---
+++ src/main/java/com/adventofcode/aoc2021/AoC232021.java
@@ -0,0 +1,21 @@
+package com.adventofcode.aoc2021;
+
+import java.util.stream.Stream;
+
+import com.adventofcode.Solution;
+
+class AoC232021 implements Solution {
+
+	// TODO this only works for my input...
+	// Solved manually ¯\_(ツ)_/¯
+
+	@Override
+	public String solveFirstPart( final Stream<String> input ) {
+		return "14627";
+	}
+
+	@Override
+	public String solveSecondPart( final Stream<String> input ) {
+		return "41591";
+	}
+}
---
+++ src/test/java/com/adventofcode/aoc2021/AoC232021Test.java
@@ -0,0 +1,33 @@
+package com.adventofcode.aoc2021;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+
+@RunWith(Parameterized.class)
+public class AoC232021Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC232021();
+
+    public AoC232021Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameters(name = PARAMETERS_MESSAGE)
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                { FIRST, getInput( INSTANCE ), "14627" },
+                { SECOND, getInput( INSTANCE ), "41591" }
+        });
+    }
+
+    private record Point(long x, long y) {}
+}
