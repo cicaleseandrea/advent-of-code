93708ecb2bca1d63b9d39729fdb4fa5167fb02a8
Andrea Cicalese <49196624+cicaleseandrea@users.noreply.github.com>
2023-01-14 23:11:21 +0000

    day 14 2016

---
+++ src/main/java/com/adventofcode/aoc2016/AoC142016.java
@@ -0,0 +1,79 @@
+package com.adventofcode.aoc2016;
+
+import static com.adventofcode.utils.Utils.getFirstString;
+import static com.adventofcode.utils.Utils.itoa;
+
+import com.adventofcode.Solution;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.hash.Hashing;
+import com.google.common.io.BaseEncoding;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class AoC142016 implements Solution {
+
+  private static final Pattern REGEX_THREE = Pattern.compile( "(.)\\1{2}" );
+  private static final Pattern REGEX_FIVE = Pattern.compile( "(.)\\1{4}" );
+
+  @Override
+  public String solveFirstPart(final Stream<String> input) {
+    return solve( input, true );
+  }
+
+  @Override
+  public String solveSecondPart(final Stream<String> input) {
+    return solve( input, false );
+  }
+
+  private String solve(final Stream<String> input, final boolean first) {
+    final var salt = getFirstString( input );
+    final Multimap<String, Character> fives = HashMultimap.create();
+    final Queue<String> hashes = new LinkedList<>();
+
+    // initialization
+    for ( int i = 0; i < 1000; i++ ) {
+      addHash( salt, i, hashes, fives, first );
+    }
+
+    for ( int i = 0, keys = 0; ; i++ ) {
+      addHash( salt, i + 1000, hashes, fives, first );
+      final var hash = hashes.poll();
+      fives.removeAll( hash );
+      if ( fives.containsValue( computeThree( hash ) ) && ++keys == 64 ) {
+        return itoa( i );
+      }
+    }
+  }
+
+  private void addHash(final String salt, final int i, final Queue<String> hashes,
+      final Multimap<String, Character> fives, final boolean first) {
+    String hash = salt + i;
+    for ( int j = 0; j < (first ? 1 : 2017); j++ ) {
+      hash = computeHash( hash );
+    }
+    hashes.add( hash );
+    fives.putAll( hash, computeFives( hash ) );
+  }
+
+  private Character computeThree(final String hash) {
+    final var matcher = REGEX_THREE.matcher( hash );
+    return matcher.find() ? matcher.group( 1 ).charAt( 0 ) : null;
+  }
+
+  private Set<Character> computeFives(final String hash) {
+    final var matcher = REGEX_FIVE.matcher( hash );
+    return matcher.results().map( r -> r.group( 1 ).charAt( 0 ) ).collect( Collectors.toSet() );
+  }
+
+  @SuppressWarnings("deprecation")
+  private String computeHash(final CharSequence message) {
+    final byte[] md5 = Hashing.md5().hashString( message, StandardCharsets.UTF_8 ).asBytes();
+    return BaseEncoding.base16().lowerCase().encode( md5 );
+  }
+}
---
+++ src/test/java/com/adventofcode/aoc2016/AoC142016Test.java
@@ -0,0 +1,30 @@
+package com.adventofcode.aoc2016;
+
+import static com.adventofcode.Generic.Type.FIRST;
+import static com.adventofcode.Generic.Type.SECOND;
+
+import com.adventofcode.Generic;
+import com.adventofcode.Solution;
+import java.util.List;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class AoC142016Test extends Generic {
+
+    private static final Solution INSTANCE = new AoC142016();
+
+    public AoC142016Test(final Type type, final String input, final String result) {
+        super(INSTANCE, type, input, result);
+    }
+
+    @Parameters(name = "{0}({index})->{2}")
+    public static Iterable<Object[]> data() {
+        return List.of(new Object[][]{
+                { FIRST, "abc", "22728" },
+                { FIRST, getInput( INSTANCE ), "35186" },
+                { SECOND, getInput( INSTANCE ), "22429" },
+        });
+    }
+}
