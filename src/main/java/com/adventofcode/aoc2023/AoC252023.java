047e9ea00c005352aa692ed8cd8f7c87828d42ec
Andrea Cicalese <49196624+cicaleseandrea@users.noreply.github.com>
2023-12-26 09:25:10 +0000

    day 25 2023 some refactoring

--- src/main/java/com/adventofcode/aoc2023/AoC252023.java
+++ src/main/java/com/adventofcode/aoc2023/AoC252023.java
@@ -2,7 +2,6 @@ package com.adventofcode.aoc2023;
 
 import static com.adventofcode.utils.Utils.MERRY_CHRISTMAS;
 import static com.adventofcode.utils.Utils.itoa;
-import static java.util.stream.Collectors.toSet;
 
 import com.adventofcode.Solution;
 import com.adventofcode.utils.DisjointSet;
@@ -10,21 +9,20 @@ import com.adventofcode.utils.DisjointSet.Node;
 import com.adventofcode.utils.Utils;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 
 class AoC252023 implements Solution {
 
+  private static final int MIN_CUT_SIZE = 3;
+
   @Override
   public String solveFirstPart(final Stream<String> input) {
     final List<Edge> wires = getEdges( input );
-    final Set<String> components = wires.stream().flatMap( e -> Stream.of( e.a, e.b ) )
-        .collect( toSet() );
 
     Optional<Long> result = Optional.empty();
     while ( result.isEmpty() ) {
-      result = computeResult( components, wires );
+      result = computeResult( wires );
     }
     return itoa( result.orElseThrow() );
   }
@@ -36,42 +34,37 @@ class AoC252023 implements Solution {
         .toList();
   }
 
-  private static Optional<Long> computeResult(final Set<String> components,
-      final List<Edge> wires) {
+  private static Optional<Long> computeResult(final List<Edge> wires) {
     //create disjoint sets
     final DisjointSet<String> subgraphs = new DisjointSet<>();
-    components.forEach( subgraphs::makeSet );
+    wires.stream().flatMap( e -> Stream.of( e.a, e.b ) ).forEach( subgraphs::makeSet );
 
     //1. find a cut
-    final int nWires = wires.size();
-    int nSubgraphs = components.size();
     //continue until there are only two subgraphs
-    while ( nSubgraphs > 2 ) {
+    while ( subgraphs.getSize() > 2 ) {
       //choose an edge at random
-      final Edge wire = wires.get( ThreadLocalRandom.current().nextInt( nWires ) );
+      final Edge wire = wires.get( ThreadLocalRandom.current().nextInt( wires.size() ) );
       //contract the edge
-      if ( subgraphs.union( wire.a, wire.b ) ) {
-        nSubgraphs--;
-      }
+      subgraphs.union( wire.a, wire.b );
     }
 
-    //2. check if cut is minimum
-    int nWiresToDisconnect = 0;
-    long result = 0;
-    for ( final Edge wire : wires ) {
+    //2. find edges crossing the cut
+    final List<Edge> cutWires = wires.stream().filter( wire -> {
       final Node<String> subgraphA = subgraphs.find( wire.a );
       final Node<String> subgraphB = subgraphs.find( wire.b );
-      if ( !subgraphA.equals( subgraphB ) ) {
-        //this wire crosses the cut
-        nWiresToDisconnect++;
-        if ( nWiresToDisconnect > 3 ) {
-          //this cut is not minimum
-          return Optional.empty();
-        }
-        result = (long) subgraphA.getSize() * subgraphB.getSize();
-      }
+      return !subgraphA.equals( subgraphB );
+    } ).limit( MIN_CUT_SIZE + 1L ).toList();
+
+    //3. check if cut is minimum
+    if ( cutWires.size() > MIN_CUT_SIZE ) {
+      return Optional.empty();
     }
-    return Optional.of( result );
+
+    //4. compute result
+    final Edge wireToDisconnect = cutWires.get( 0 );
+    final Node<String> subgraphA = subgraphs.find( wireToDisconnect.a );
+    final Node<String> subgraphB = subgraphs.find( wireToDisconnect.b );
+    return Optional.of( (long) subgraphA.getSize() * subgraphB.getSize() );
   }
 
   @Override
