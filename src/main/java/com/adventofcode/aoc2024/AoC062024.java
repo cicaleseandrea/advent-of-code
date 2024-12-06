2434db4a006c7a2af1dbe11c8992cc89ea1ed04d
Andrea Cicalese <49196624+cicaleseandrea@users.noreply.github.com>
2024-12-06 17:56:27 +0000

    day 6 speedup

--- src/main/java/com/adventofcode/aoc2024/AoC062024.java
+++ src/main/java/com/adventofcode/aoc2024/AoC062024.java
@@ -4,12 +4,13 @@ import static com.adventofcode.utils.Direction.UP;
 import static com.adventofcode.utils.Utils.DOT;
 import static com.adventofcode.utils.Utils.HASH;
 import static com.adventofcode.utils.Utils.itoa;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.adventofcode.Solution;
 import com.adventofcode.utils.Direction;
 import com.adventofcode.utils.Pair;
 import com.adventofcode.utils.Utils;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -32,48 +33,48 @@ class AoC062024 implements Solution {
   private String solve(final Stream<String> input, final boolean first) {
     List<List<Character>> grid = Utils.getCharMatrix( input );
     Pair<Long, Long> start = getStart( grid );
+    List<Pair<Long, Long>> path = checkNotNull( moveGuard( start, grid, true ) );
     if ( first ) {
-      //count distinct positions
-      return itoa( moveGuard( start, grid, true ) );
+      return itoa( path.size() );
     }
+
     int result = 0;
-    for ( int i = 0; i < grid.size(); i++ ) {
-      for ( int j = 0; j < grid.get( 0 ).size(); j++ ) {
-        if ( grid.get( i ).get( j ) == EMPTY ) {
-          //add obstacle
-          grid.get( i ).set( j, OBSTACLE );
-          if ( moveGuard( start, grid, false ) == -1 ) {
-            //found a loop
-            result++;
-          }
-          //remove obstacle
-          grid.get( i ).set( j, EMPTY );
-        }
+    //consider all positions except the starting one
+    for ( Pair<Long, Long> position : path.subList( 1, path.size() ) ) {
+      int i = position.getFirst().intValue();
+      int j = position.getSecond().intValue();
+      //add obstacle
+      grid.get( i ).set( j, OBSTACLE );
+      if ( moveGuard( start, grid, false ) == null ) {
+        //found a loop
+        result++;
       }
+      //remove obstacle
+      grid.get( i ).set( j, EMPTY );
     }
     return itoa( result );
   }
 
   /**
-   * Simulate movement. Return -1 if there is a loop. Otherwise, return 0 if count is false,
-   * otherwise return the number of distinct positions
+   * Simulate movement. Return null if there is a loop. Otherwise, return the full path if fullPath
+   * is true, otherwise return an empty list
    *
    * @param position starting position
    * @param grid     grid with obstacles and starting position
-   * @param count    enable counting distinct positions
-   * @return -1 if there is a loop, otherwise 0 if count is false, otherwise number of distinct
-   * positions
+   * @param fullPath enable returning the full path
+   * @return null if there is a loop, otherwise all positions explored if fullPath is true,
+   * otherwise empty list
    */
-  private long moveGuard(Pair<Long, Long> position, final List<List<Character>> grid,
-      boolean count) {
+  private List<Pair<Long, Long>> moveGuard(Pair<Long, Long> position,
+      final List<List<Character>> grid, boolean fullPath) {
     Direction direction = UP;
-    Set<State> states = new HashSet<>();
+    Set<State> states = new LinkedHashSet<>();
     states.add( new State( position, direction ) );
     while ( true ) {
       Pair<Long, Long> nextPosition = getNextPosition( position, direction );
       if ( isOutsideGrid( nextPosition, grid ) ) {
         //leave the area
-        return count ? states.stream().map( State::position ).distinct().count() : 0;
+        return fullPath ? states.stream().map( State::position ).distinct().toList() : List.of();
       } else {
         char nextSymbol = grid.get( nextPosition.getFirst().intValue() )
             .get( nextPosition.getSecond().intValue() );
@@ -82,12 +83,12 @@ class AoC062024 implements Solution {
           direction = direction.rotateClockwise();
           if ( !states.add( new State( position, direction ) ) ) {
             //was here with same direction before: found a loop
-            return -1;
+            return null;
           }
         } else {
           //move
           position = nextPosition;
-          if ( count ) { //conditional to speedup part 2
+          if ( fullPath ) { //conditional to speedup part 2
             states.add( new State( position, direction ) );
           }
         }
