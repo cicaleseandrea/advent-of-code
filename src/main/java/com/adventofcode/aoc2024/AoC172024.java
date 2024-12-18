9ce90ada62280402646ba0eeb31b783afd882f80
Andrea Cicalese <49196624+cicaleseandrea@users.noreply.github.com>
2024-12-18 23:02:56 +0000

    day 17 cleanup

--- src/main/java/com/adventofcode/aoc2024/AoC172024.java
+++ src/main/java/com/adventofcode/aoc2024/AoC172024.java
@@ -1,16 +1,14 @@
 package com.adventofcode.aoc2024;
 
-import static com.adventofcode.utils.Computer2024.EIGHT;
-import static java.math.BigInteger.ONE;
-import static java.math.BigInteger.ZERO;
+
+import static com.adventofcode.utils.Utils.itoa;
+import static java.util.stream.Collectors.joining;
 
 import com.adventofcode.Solution;
 import com.adventofcode.utils.Computer2024;
 import com.adventofcode.utils.Utils;
 import com.google.common.base.Preconditions;
-import java.math.BigInteger;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class AoC172024 implements Solution {
@@ -27,37 +25,37 @@ class AoC172024 implements Solution {
 
   private String solve(final Stream<String> input, final boolean first) {
     List<List<Long>> numbers = input.map( Utils::toLongList ).toList();
-    List<Integer> instructions = numbers.get( 4 ).stream().map( Number::intValue ).toList();
-    BigInteger a = BigInteger.valueOf( numbers.get( 0 ).get( 0 ) );
-    if ( first ) {
-      List<Integer> output = runComputer( instructions, a );
-      return output.stream().map( Object::toString ).collect( Collectors.joining( "," ) );
-    }
-    a = ZERO;
+    List<Integer> instructions = numbers.get( 4 ).stream().map( Long::intValue ).toList();
+    long a = first ? numbers.get( 0 ).get( 0 ) : findQuine( instructions );
+    List<Integer> output = runComputer( instructions, a );
+    Preconditions.checkArgument( first || output.equals( instructions ),
+        "output for a=" + a + ": " + output + " instructions: " + instructions );
+    return first ? output.stream().map( Object::toString ).collect( joining( "," ) ) : itoa( a );
+  }
+
+  private long findQuine(final List<Integer> instructions) {
+    long a = 0;
     boolean backtrack = false;
     for ( int i = instructions.size() - 1; i >= 0; i-- ) {
-      a = backtrack ? a.divide( EIGHT ) : a.multiply( EIGHT );
+      a = backtrack ? a / 8 : a * 8;
       int digitWanted = instructions.get( i );
       int offset = 0;
       while ( runComputer( instructions, a ).get( 0 ) != digitWanted ) {
-        a = a.add( ONE );
+        a++;
         offset++;
       }
-      if ( offset < 8 ) {
-        backtrack = false;
-      } else {
+      if ( offset >= 8 ) {
         //crossed the boundary. backtrack to the previous digit
         i += 2;
         backtrack = true;
+      } else {
+        backtrack = false;
       }
     }
-    List<Integer> output = runComputer( instructions, a );
-    Preconditions.checkArgument( output.equals( instructions ),
-        "output for a=" + a + ": " + output + " instructions: " + instructions );
-    return a.toString();
+    return a;
   }
 
-  private List<Integer> runComputer(final List<Integer> instructions, final BigInteger a) {
+  private List<Integer> runComputer(final List<Integer> instructions, final long a) {
     var computer = new Computer2024( instructions, a );
     while ( computer.step() ) {
       //keep running
--- src/main/java/com/adventofcode/utils/Computer2024.java
+++ src/main/java/com/adventofcode/utils/Computer2024.java
@@ -1,76 +1,52 @@
 package com.adventofcode.utils;
 
-import static java.math.BigInteger.ZERO;
-
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer2024 {
 
-  public static final BigInteger EIGHT = BigInteger.valueOf( 8 );
   private int instrPointer;
   private final int[] instructions;
-  private final BigInteger[] registers;
+  private final long[] registers;
   private final List<Integer> output = new ArrayList<>();
 
-  public Computer2024(final List<Integer> instructions, final BigInteger a) {
+  public Computer2024(final List<Integer> instructions, final long a) {
     this.instrPointer = 0;
     this.instructions = instructions.stream().mapToInt( Integer::intValue ).toArray();
-    this.registers = new BigInteger[]{a, ZERO, ZERO};
+    this.registers = new long[]{a, 0, 0};
   }
 
   public boolean step() {
     if ( instrPointer >= instructions.length ) {
       return false;
     }
-    int opCode = getInstruction();
+    int opCode = instructions[instrPointer];
     switch ( opCode ) {
-      case 0 -> registers[0] = division();
-      case 1 -> registers[1] = xor( BigInteger.valueOf( getLiteralOperand() ) );
-      case 2 -> registers[1] = getComboOperandMod8();
-      case 3 -> {
-        if ( !registers[0].equals( ZERO ) ) {
-          instrPointer = getLiteralOperand() - 2;
-        }
-      }
-      case 4 -> registers[1] = xor( registers[2] );
-      case 5 -> output.add( getComboOperandMod8().intValue() );
-      case 6 -> registers[1] = division();
-      case 7 -> registers[2] = division();
+      case 0 -> registers[0] = registers[0] >> getComboOperand();
+      case 1 -> registers[1] ^= getLiteralOperand();
+      case 2 -> registers[1] = getComboOperand() % 8;
+      case 3 -> instrPointer = registers[0] != 0 ? getLiteralOperand() - 2 : instrPointer;
+      case 4 -> registers[1] ^= registers[2];
+      case 5 -> output.add( (int) (getComboOperand() % 8) );
+      case 6 -> registers[1] = registers[0] >> getComboOperand();
+      case 7 -> registers[2] = registers[0] >> getComboOperand();
       default -> throw new IllegalStateException( "Unexpected opCode: " + opCode );
     }
     instrPointer += 2;
     return true;
   }
 
-  private BigInteger xor(final BigInteger operand) {
-    return registers[1].xor( operand );
-  }
-
-  private BigInteger division() {
-    BigInteger num = registers[0];
-    BigInteger den = BigInteger.TWO
-        .pow( getComboOperandMod8().intValue() );
-    return num.divide( den );
-  }
-
-  private int getInstruction() {
-    return instructions[instrPointer];
-  }
-
   private int getLiteralOperand() {
     return instructions[instrPointer + 1];
   }
 
-  private BigInteger getComboOperandMod8() {
+  private long getComboOperand() {
     int operand = getLiteralOperand();
-    BigInteger comboOperand = switch ( operand ) {
-      case 0, 1, 2, 3 -> BigInteger.valueOf( operand );
+    return switch ( operand ) {
+      case 0, 1, 2, 3 -> operand;
       case 4, 5, 6 -> registers[operand - 4];
-      default -> throw new IllegalStateException( "Unexpected comboOp: " + operand );
+      default -> throw new IllegalStateException( "Unexpected combo operand: " + operand );
     };
-    return comboOperand.mod( EIGHT );
   }
 
   public List<Integer> getOutput() {
