a222365d7994961323b6deeca8c1d64d65e7f43d
cicaleseandrea <49196624+cicaleseandrea@users.noreply.github.com>
2025-12-07 15:21:38 +0000

    cleanup

--- src/main/java/com/adventofcode/aoc2025/AoC072025.java
+++ src/main/java/com/adventofcode/aoc2025/AoC072025.java
@@ -6,10 +6,12 @@ import com.adventofcode.Solution;
 import java.util.stream.Stream;
 
 class AoC072025 implements Solution {
-  private static final char START = 'S';
-  private static final char SPLITTER = '^';
-  private static final long SPLITTER_NUM = -1;
-  private static final long BEAM_NUM = 1;
+
+  private static final char START_SYMBOL = 'S';
+  private static final char SPLITTER_SYMBOL = '^';
+  private static final long EMPTY = 0;
+  private static final long BEAM = 1;
+  private static final long SPLITTER = -1;
 
   @Override
   public String solveFirstPart(Stream<String> input) {
@@ -26,13 +28,14 @@ class AoC072025 implements Solution {
     int splits = 0;
     for (int i = 0; i < grid.length - 1; i++) {
       for (int j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] >= BEAM_NUM) {
-          if (grid[i + 1][j] == SPLITTER_NUM) {
-            grid[i + 1][j - 1] += grid[i][j];
-            grid[i + 1][j + 1] += grid[i][j];
+        long curr = grid[i][j];
+        if (curr >= BEAM) {
+          if (grid[i + 1][j] == SPLITTER) {
+            grid[i + 1][j - 1] += curr;
+            grid[i + 1][j + 1] += curr;
             splits++;
           } else {
-            grid[i + 1][j] += grid[i][j];
+            grid[i + 1][j] += curr;
           }
         }
       }
@@ -50,9 +53,9 @@ class AoC072025 implements Solution {
             row.chars()
                 .mapToObj(c ->
                     switch (c) {
-                      case START -> BEAM_NUM;
-                      case SPLITTER -> SPLITTER_NUM;
-                      default -> 0L;
+                      case START_SYMBOL -> BEAM;
+                      case SPLITTER_SYMBOL -> SPLITTER;
+                      default -> EMPTY;
                     })
                 .toArray(Long[]::new))
         .toArray(Long[][]::new);
