dede674234be6385f71891171a5b01c7c1eac909
cicaleseandrea <49196624+cicaleseandrea@users.noreply.github.com>
2025-12-11 09:42:35 +0000

    speed up

--- src/main/java/com/adventofcode/aoc2025/AoC112025.java
+++ src/main/java/com/adventofcode/aoc2025/AoC112025.java
@@ -3,10 +3,13 @@ package com.adventofcode.aoc2025;
 import static com.adventofcode.utils.Utils.itoa;
 
 import com.adventofcode.Solution;
+import com.adventofcode.utils.Pair;
 import com.adventofcode.utils.Utils;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 class AoC112025 implements Solution {
@@ -31,53 +34,44 @@ class AoC112025 implements Solution {
     ListMultimap<String, String> connections = ArrayListMultimap.create();
     input.forEach(line -> {
       List<String> wordList = Utils.toWordList(line);
-      wordList.subList(1, wordList.size()).forEach(word -> {
-        connections.put(wordList.get(0), word);
-      });
+      wordList.subList(1, wordList.size())
+          .forEach(word -> connections.put(wordList.get(0), word));
     });
+    
     if (first) {
-      return itoa(countAllPaths(START_FIRST_PART, END, connections, Integer.MAX_VALUE));
+      return itoa(countAllPaths(START_FIRST_PART, END, connections));
     } else {
-      //TODO remove hardcoded max length
-      long svr_dac = countAllPaths(START_SECOND_PART, MUST_VISIT1, connections, 8);
-      long dac_fft = 0;
-      long fft_end = 0;
-      if (svr_dac > 0) {
-        dac_fft = countAllPaths(MUST_VISIT1, MUST_VISIT2, connections, 16);
-        fft_end = countAllPaths(MUST_VISIT2, END, connections, 9);
-      }
-      long svr_fft = countAllPaths(START_SECOND_PART, MUST_VISIT2, connections, 8);
-      System.out.println(svr_fft);
-      long fft_dac = 0;
-      long dac_end = 0;
-      if (svr_fft > 0) {
-        fft_dac = countAllPaths(MUST_VISIT2, MUST_VISIT1, connections, 16);
-        System.out.println(fft_dac);
-        dac_end = countAllPaths(MUST_VISIT1, END, connections, 9);
-        System.out.println(dac_end);
-      }
-      long one = svr_dac * dac_fft * fft_end;
-      long two = svr_fft * fft_dac * dac_end;
-      return itoa(one + two);
+      long result = 0;
+      long svrDac = countAllPaths(START_SECOND_PART, MUST_VISIT1, connections);
+      long dacFft = countAllPaths(MUST_VISIT1, MUST_VISIT2, connections);
+      long fftOut = countAllPaths(MUST_VISIT2, END, connections);
+      result += (svrDac * dacFft * fftOut);
+      long svrFft = countAllPaths(START_SECOND_PART, MUST_VISIT2, connections);
+      long fftDac = countAllPaths(MUST_VISIT2, MUST_VISIT1, connections);
+      long dacOut = countAllPaths(MUST_VISIT1, END, connections);
+      result += (svrFft * fftDac * dacOut);
+      return itoa(result);
     }
   }
 
-  private long countAllPaths(String start, String end, ListMultimap<String, String> connections, int maxLength) {
-    return countAllPaths(start, end, connections, 0, maxLength);
+  private long countAllPaths(String start, String end, ListMultimap<String, String> connections) {
+    return countAllPaths(start, end, connections, new HashMap<>());
   }
 
-  private long countAllPaths(String start, String end, ListMultimap<String, String> connections, int currPathLength, int maxLength) {
-    if (currPathLength > maxLength) {
-      return 0;
+  private int countAllPaths(String start, String end, ListMultimap<String, String> connections, Map<Pair<String, String>, Integer> cache) {
+    Pair<String, String> path = new Pair<>(start, end);
+    if (cache.containsKey(path)) {
+      return cache.get(path);
     }
-    long count = 0;
+    int count = 0;
     for (String neighbour : connections.get(start)) {
       if (neighbour.equals(end)) {
         count++;
       } else {
-        count += countAllPaths(neighbour, end, connections, currPathLength + 1, maxLength);
+        count += countAllPaths(neighbour, end, connections, cache);
       }
     }
+    cache.put(path, count);
     return count;
   }
 }
